5884841841f2ca7c5c58278f2afd1e4b035ecd70
DeHeerGreven <93603732+DeHeerGreven@users.noreply.github.com>
2022-04-06 10:02:09 +0200

    header php toegevoegd

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+    <div class ="header-menu">
+        <div class = "wrapper">
+            <h1>Welkom!</h1>
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="contact.php">Contact</a>
+            </nav>
+        </div> 
+    </div>
+</header> 
--- index.php
+++ index.php
@@ -4,23 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="css/main.css">
     <title>Rollercoaster</title>
 </head>
 <body>
     <main>
         <div class ="main-menu">
             <div class ="wrapper">
-                <header>
-                    <div class ="header-menu">
-                        <div class = "wrapper">
-                            <h1>Welkom!</h1>
-                            <nav>
-                                <a href="">Home</a>
-                            </nav>
-                        </div> 
-                    </div>
-                </header>                         
+                        
             </div>
         </div>
     </main>
