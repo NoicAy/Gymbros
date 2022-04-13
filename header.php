02c820331457736e1b0293f2ff139d92c887ccb4
Japie-vE <japievaneijk@live.nl>
2022-04-13 09:47:44 +0200

    header h1 aangepast en 2 pagina's toegevoegd aan de nav

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
 <header>
     <div class ="header-menu">
         <div class = "wrapper">
-            <h1>Welkom!</h1>
+            <h1>Supercoaster!</h1>
             <nav>
                 <a href="index.php">Home</a>
+                <a href="constructie.php">Constructie</a>
+                <a href="karretjes.php">Karretjes</a>
                 <a href="contact.php">Contact</a>
             </nav>
         </div> 
