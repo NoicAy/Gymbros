a57336cec39f9d4eb3f88b6b594feeb64a9c35da
DeHeerGreven <93603732+DeHeerGreven@users.noreply.github.com>
2022-04-13 10:17:47 +0200

    img toegevoegd op home

--- index.php
+++ index.php
@@ -27,12 +27,15 @@
 
        
        <div class = "wrapper">
-        <h1> <span id="achtbaanSpan">Home</h1>
-            <div class = "achtbaanBox">
-                
-                <h3>MagnaDrive</h3>
-                <p>Onze nieuwe achtbaantechniek heet MagnaDrive. Door middel van een aantal onderdelen die ons ontwikkelteam uitgevonden heeft, kunnen we een achtbaan produceren die tot 5-10% sneller kan gaan dan de snelste achtbaan tot nu toe, enkele graden scherpte kan toevoegen aan bochten, en ook nog langer meegaat dan achtbanen gemaakt van gebruikelijke onderdelen. 
-                Het mooiste is nog dat de onderdelen niet veel duurder zullen zijn dan onze concurrentie. Vanwege de scherpere bochten is in veel gevallen ook de achtbaan compacter te maken, wat ook weer ruimte vrijmaakt voor andere attracties.</p>
+           <div class ="achtbaanBox2">
+                <h1>Home</h1>
+                    <div class = "achtbaanBox">
+                        
+                        <h3>MagnaDrive</h3>
+                        <p>Onze nieuwe achtbaantechniek heet MagnaDrive. Door middel van een aantal onderdelen die ons ontwikkelteam uitgevonden heeft, kunnen we een achtbaan produceren die tot 5-10% sneller kan gaan dan de snelste achtbaan tot nu toe, enkele graden scherpte kan toevoegen aan bochten, en ook nog langer meegaat dan achtbanen gemaakt van gebruikelijke onderdelen. 
+                        Het mooiste is nog dat de onderdelen niet veel duurder zullen zijn dan onze concurrentie. Vanwege de scherpere bochten is in veel gevallen ook de achtbaan compacter te maken, wat ook weer ruimte vrijmaakt voor andere attracties.</p>
+                    </div>
+                    <img src="img/plaatje3.jpg">
             </div>
     </main>
 </body>
