e9f9be22978ddae7952876f8c2d4f9352d6e5ec4
Japie-vE <japievaneijk@live.nl>
2022-04-13 09:45:44 +0200

    Constructie en karretjes pagina aangemaakt

---
+++ karretjes.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Rollercoaster</title>
+</head>
+<body>
+
+<?php
+
+  require_once('header.php');
+
+?>
+
+    <main>
+        <div class ="main-menu">
+            <div class ="wrapper">
+            
+            </div>
+        </div>
+    </main>
+</body>
+<?php
+
+  require_once('footer.php');
+
+?>
+</html>
