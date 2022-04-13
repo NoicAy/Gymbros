67a6ab06c35a5cd359501bbe887f082ff6984a0e
DeHeerGreven <93603732+DeHeerGreven@users.noreply.github.com>
2022-04-13 10:08:17 +0200

    class achtbaanBox en informatie over techniek toegevoegd

--- index.php
+++ index.php
@@ -27,11 +27,12 @@
 
        
        <div class = "wrapper">
-        <h1> <span id="serV">Ser</span>vices</h1>
-            <div class = "serviceBox">
+        <h1> <span id="achtbaanSpan">Home</h1>
+            <div class = "achtbaanBox">
                 
-                <h3>Coffee</h3>
-                <p><span id = "coffeeBox">Lorem, ipsum dolor, sit amet consectetur adipisicing elit. Officiis hic porro quis sed vel, sunt, vero nam rerum, eum quae consectetur. Sint nesciunt voluptatem voluptate natus, ex ab laborum deserunt.</span></p>
+                <h3>MagnaDrive</h3>
+                <p>Onze nieuwe achtbaantechniek heet MagnaDrive. Door middel van een aantal onderdelen die ons ontwikkelteam uitgevonden heeft, kunnen we een achtbaan produceren die tot 5-10% sneller kan gaan dan de snelste achtbaan tot nu toe, enkele graden scherpte kan toevoegen aan bochten, en ook nog langer meegaat dan achtbanen gemaakt van gebruikelijke onderdelen. 
+                Het mooiste is nog dat de onderdelen niet veel duurder zullen zijn dan onze concurrentie. Vanwege de scherpere bochten is in veel gevallen ook de achtbaan compacter te maken, wat ook weer ruimte vrijmaakt voor andere attracties.</p>
             </div>
     </main>
 </body>
