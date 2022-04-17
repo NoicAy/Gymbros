89c6a969c1698b1157889147ecfc93daf5caa1d2
Japie-vE <japievaneijk@live.nl>
2022-04-17 14:10:17 +0200

    Contact pagina bijgewerkt

--- contact.php
+++ contact.php
@@ -13,23 +13,26 @@
 
     
     <div class="container">
-    <form action="backend/rollercoasterController.php" method="post">
-        <input type="hidden" name="action" value="contact">
-            <div class="style">
-                <div class="wrapper">
-                    <div class="form">
-                        <h2 class="contact">Contact</h2>
-                        <label for="fname">Volledige naam: </label>
-                        <input type="text" id="fname" name="fullname"><br>
-                        <label for="email">E-mail: </label>
-                        <input type="email" name="email"><br>
-                        <label for="message">Bericht: </label><br>
-                        <textarea name="message" id="message" cols="100" rows="10"></textarea>
-                        <hr>
-                        <input type="submit" id="submit" value="Indienen">
+        <div class="contact">
+            <form action="backend/rollercoasterController.php" method="post">
+                <input type="hidden" name="action" value="contact">
+                    <div class="style">
+                        <div class="wrapper">
+                            <div class="form">
+                                <h2 class="contact">Contact</h2>
+                                <label for="fname">Volledige naam: </label>
+                                <input type="text" id="fname" name="fullname"><br>
+                                <label for="email">E-mail: </label>
+                                <input type="email" name="email"><br>
+                                <label for="message">Bericht: </label><br>
+                                <textarea name="message" id="message" cols="100" rows="10"></textarea>
+                                <hr>
+                                <input type="submit" id="submit" value="Indienen">
+                            </div>
+                        </div>
                     </div>
-                </div>
-    </div>
+            </div>
+</div>
 
 </body>
 <?php
