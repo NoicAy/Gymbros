d06a9aa67e122562fdc2f8c14d77d9d4ab35731a
Japie-vE <japievaneijk@live.nl>
2022-04-17 14:09:38 +0200

    Karretjes en Constructie pagina afgerond

--- karretjes.php
+++ karretjes.php
@@ -16,11 +16,21 @@
 ?>
 
     <main>
-        <div class ="main-menu">
-            <div class ="wrapper">
-            
+          <div class ="wrapper">
+            <div class ="achtbaanBox2">
+                <h1>Karretjes</h1>
+                    <div class = "achtbaanBox">
+                        
+                        <h3>MagnaCarts</h3>
+                        <p>Met onze MagnaCarts die ook speciaal werken op onze nieuw ontwikkelde MagnaAlloy onderdelen, kan elk pretpark het chassis naar wens aanpassen. 
+                          Wij hebben een frame ontwikkeld die wij zo klein mogelijk hebben gehouden, zodat de rest van de kar helemaal zelf door het team ontworpen en ontwikkeld kan worden. 
+                          Ook leveren wij de digitale bestanden waar deze ontwerpen op gebaseerd kunnen worden, zodat het team zo snel mogelijk aan de slag kan gaan unieke ontwerpen te creëren. 
+                          Omdat ook deze karretjes gebruik maken van MagnaAlloy, zijn ze dus ook bestand tegen de extra G-krachten die deze techniek mogelijk maakt. 
+                          Zo zijn onze producten dus breed inzetbaar, en zorgen wij voor de beste vooruitgang in de achtbaantechniek van het afgelopen decennia.</p>
+                    </div>
+                    <img src="img/plaatje7.jpg">
             </div>
-        </div>
+          </div>
     </main>
 </body>
 <?php
